54e9ba070f34416937409aee5b5bbf58e1c12b14
Adi Latman <adi_latman01@gmail.com>
2026-05-20 12:12:15 +0300

    Added responsive GitHub link exclusively on the login screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import { resetAuthOnPageLoad } from './api'
@@ -9,5 +10,6 @@ resetAuthOnPageLoad()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
